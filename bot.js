b30b95a65d45d5ed08469e7020a6a78774772c5b
gekyumeee <45327532+gekyumeee@users.noreply.github.com>
2018-11-25 20:54:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,82 +81,8 @@ if (message.content === '!spam') {
       }
 });
 
-client3.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(` [ " ${x} " ]علشان انا جيكومي فشيخ نيك يا ولاد المتناكه`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client4.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(` [ " ${x} " ]علشان انا جيكومي فشيخ نيك يا ولاد المتناكه`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client5.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(` [ " ${x} " ]علشان انا جيكومي فشيخ نيك يا ولاد المتناكه`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client6.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(` [ " ${x} " ]علشان انا جيكومي فشيخ نيك يا ولاد المتناكه`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client7.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(` [ " ${x} " ]علشان انا جيكومي فشيخ نيك يا ولاد المتناكه`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
 
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
-client3.login(process.env.TOKEN3);// لا تغير فيها شيء
-client4.login(process.env.TOKEN4);// لا تغير فيها شيء
-client5.login(process.env.TOKEN5);// لا تغير فيها شيء
-client6.login(process.env.TOKEN6);// لا تغير فيها شيء
-client7.login(process.env.TOKEN7);// لا تغير فيها شيء
