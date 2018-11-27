b0e58985c85ad8e89603c9671c1247cdcc49a010
gekyumeee <45327532+gekyumeee@users.noreply.github.com>
2018-11-27 05:38:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(` [ " ${x} " ]علشان انا جيكومي فشيخ نيك يا ولاد المتناكه`)
+        message.channel.send(` #credits @heijaxxxo 5000`)
           .then(m => {
             count++;
           })
@@ -72,7 +72,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(` [ " ${x} " ]علشان انا جيكومي فشيخ نيك يا ولاد المتناكه`)
+        message.channel.send(` #credits @heijaxxxo 5000`)
           .then(m => {
             count++;
           })
